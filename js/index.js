684b016b69c101d193894aa8cec083653cf92de6
eh-munna <emran.h.munna@gmail.com>
2024-03-06 22:01:02 +0100

    practice

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const num = '43';
+const num2 = '45';
+console.log(parseInt(num) + parseInt(num2));
+
+const num3 = '45';
+const num4 = 43 - 2;
+const num5 = 'hello';
+console.log(Number.isNaN(num3));
+console.log(Number.isNaN(num4));
+console.log(isNaN(num5));
+
+console.log(10 < "5");
