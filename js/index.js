77977d1fa3766997fc9d4d535c5d0ded0f8cc8cb
eh-munna <emran.h.munna@gmail.com>
2024-03-07 21:00:45 +0100

    module-18 is done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log(`test`);
+// sum up the results from 1 to 10 using while loop
+
+let sum = 0;
+let num = 1;
+let init;
+while (num <= 10) {
+  init = sum;
+  sum = sum + num;
+  console.log(init + ' + ' + num + ' = ' + sum);
+  num++;
+}
+
+console.log(`Total:`, sum);
