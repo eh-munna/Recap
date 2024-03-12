deae5a8ced3161555d7585b1c9e20b18b0b9a56a
eh-munna <emran.h.munna@gmail.com>
2024-03-12 15:04:07 +0100

    module-19.5.2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log(`test`);
+const numbers = [3, 4, 5, 6, 7, 8];
+
+let reverseNumbers = [];
+
+for (let number of numbers) {
+  reverseNumbers.unshift(number);
+  console.log(reverseNumbers);
+}
+
+console.log(reverseNumbers);
+
+const countZero = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '0') count += 1;
+  }
+  return count;
+};
+
+const result = countZero('1011010100');
+console.log(result);
