a9be6b93d91bac81e551f568a4e01a05c3c4e8ef
eh-munna <emran.h.munna@gmail.com>
2024-03-14 15:15:09 +0100

    module-22 is done

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ const getBiggestNumber = (a, b, c) => {
 };
 
 console.log(getBiggestNumber(1, 2, 3));
+
+// get the tallest height
+
+const heights = [32, 73, 7, 87, 65, 29, 64];
+
+const getTallestHeight = (heights) => {
+  let tallest = 0;
+  for (const height of heights) {
+    if (height > tallest) {
+      tallest = height;
+    }
+  }
+  return tallest;
+};
+
+console.log(getTallestHeight(heights));
