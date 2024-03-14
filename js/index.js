fdb0e32999659b2c1c12a71cf717a6db70d976c0
eh-munna <emran.h.munna@gmail.com>
2024-03-14 12:50:38 +0100

    test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log(`test`);
+// get the biggest number of 3 numbers
+
+const getBiggestNumber = (a, b, c) => {
+  if (a > b && a > c) {
+    return `${a} is bigger than ${b} and ${c}`;
+  } else if (b > a && b > c) {
+    return `${b} is bigger than ${a} and ${c}`;
+  } else {
+    return `${c} is bigger than ${a} and ${b}`;
+  }
+};
+
+console.log(getBiggestNumber(1, 2, 3));
