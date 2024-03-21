3465f7e1f42c6c3f3ddbdba8e9a59c442df9e326
eh-munna <emran.h.munna@gmail.com>
2024-03-21 11:35:09 +0100

    triangle and rectangle calculation function implemented

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log(`test`);
+function convertToNumber(id) {
+  elementField = document.querySelector(id);
+  elementFieldValue = parseFloat(elementField.value);
+  return elementFieldValue;
+}
+
+const clearInput = (id) => {
+  elementField = document.querySelector(id);
+  elementField.value = '';
+};
+
+function calculateTriangle() {
+  const triangleBaseValue = convertToNumber('#triangleBase');
+  const triangleHeightValue = convertToNumber('#triangleHeight');
+  const area = triangleBaseValue * triangleHeightValue * 0.5;
+  clearInput('#triangleBase');
+  clearInput('#triangleHeight');
+  return console.log(area);
+}
+function calculateRectangle() {
+  const rectangleWidthValue = convertToNumber('#rectangleWidth');
+  const rectangleLengthValue = convertToNumber('#rectangleLength');
+  const area = rectangleWidthValue * rectangleLengthValue;
+  clearInput('#rectangleWidth');
+  clearInput('#rectangleLength');
+  return console.log(area);
+}
