90d2b4af6e8c6529dfe5a685ca9a2698f1227867
eh-munna <emran.h.munna@gmail.com>
2024-03-27 12:25:02 +0100

    phone hunting is practiced

--- phone-hunter/js/index.js
+++ phone-hunter/js/index.js
@@ -1,5 +1,6 @@
 const phoneContainer = document.querySelector('#phoneContainer');
 const searchField = document.querySelector('#searchField');
+const loadMoreBtn = document.querySelector('#loadMoreBtn');
 
 const loadPhones = async () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
@@ -9,28 +10,20 @@ const loadPhones = async () => {
 };
 
 const displayPhones = (phones) => {
+  if (phones.length > 6) {
+    loadMoreBtn.classList.remove('hidden');
+    loadMoreBtn.classList.add('flex');
+  } else {
+    loadMoreBtn.classList.remove('flex');
+    loadMoreBtn.classList.add('hidden');
+  }
+
+  phones = phones.slice(0, 6);
+  console.log(phones.length);
   phones.forEach((phone) => {
     // console.log(phone);
     const { image, phone_name } = phone;
     const div = document.createElement('div');
-    // div.classList.add(
-    //   'card',
-    //   'w-96',
-    //   'bg-base-100',
-    //   'shadow-xl',
-    //   'shadow-yl',
-    //   'border',
-    //   'border-indigo-300',
-    //   'rounded-md',
-    //   'p-4',
-    //   'flex',
-    //   'flex-col',
-    //   'items-center',
-    //   'justify-center'
-    // );
-
-    // div.className =
-    //   'card w-96 bg-base-100 shadow-xl p-4 border-indigo-300 border flex flex-col items-center justify-center rounded-md';
 
     div.classList =
       'card w-96 bg-base-100 shadow-xl p-4 border-indigo-300 border flex flex-col items-center justify-center rounded-md';
@@ -64,8 +57,7 @@ const handleSearch = () => {
     .then((data) => {
       if (data.data.length > 0) {
         displayPhones(data.data);
-      }
-      else {
+      } else {
         phoneContainer.innerHTML = `<p>No results found for ${searchText}</p>`;
       }
     });
@@ -75,22 +67,23 @@ loadPhones();
 
 /* 
 
-<div class="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+// div.classList.add(
+    //   'card',
+    //   'w-96',
+    //   'bg-base-100',
+    //   'shadow-xl',
+    //   'shadow-yl',
+    //   'border',
+    //   'border-indigo-300',
+    //   'rounded-md',
+    //   'p-4',
+    //   'flex',
+    //   'flex-col',
+    //   'items-center',
+    //   'justify-center'
+    // );
 
-
+    // div.className =
+    //   'card w-96 bg-base-100 shadow-xl p-4 border-indigo-300 border flex flex-col items-center justify-center rounded-md';
 
 */
