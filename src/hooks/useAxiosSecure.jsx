38b00f3f1a02a1829550110d4d94dd1bd94f0310
eh-munna <emran.h.munna@gmail.com>
2024-06-22 00:56:19 +0200

    module 61 is completed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -22,7 +22,7 @@ export default function useAxiosSecure() {
           logOut()
             .then(() => {
               // Sign-out successful.
-              navigate('/');
+              navigate('/sign-in');
             })
             .catch((error) => {
               // An error happened.
